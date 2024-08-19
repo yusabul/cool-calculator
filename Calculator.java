9448c2b17030909a50804fa137b4ef9a4f407b57
yusabul <145148229+yusabul@users.noreply.github.com>
2024-08-19 13:11:21 -0400

    2nd Update Calculator.java
    Fixed issues with calculations

--- Calculator.java
+++ Calculator.java
@@ -6,76 +6,119 @@ public class Calculator {
 		boolean cont = true;
 		
 		do {
-			System.out.println("Enter + to add");
-			System.out.println("Enter - to subtract");
-			System.out.println("Enter * to multiply");
-			System.out.println("Enter / to divide");
-			System.out.println("Enter ! to exit");
+		System.out.println("Enter + to add");
+		System.out.println("Enter - to subtract");
+		System.out.println("Enter * to multiply");
+		System.out.println("Enter / to divide");
+		System.out.println("Enter ! to exit");
 		
-			char option = keyboard.next().charAt(0);
+		char option = keyboard.next().charAt(0);
 		
-			int updated = 0;
-			char save = 'y';
+		int updated = 0;
+		char save = 'y';
 		
-			if(option == '+') {
-				int num1 = addAndSub.getNumber();
-				int num2 = addAndSub.getNumber();
-				int sum = addAndSub.add(num1, num2);
-				addAndSub.print(sum);
-				while(save == 'y') {
-					System.out.println("Would you like to save this number for your next calculation? (y/n)?");
-					save = keyboard.next().charAt(0);
-					if(save != 'n') {
-						update(sum, updated);
+		if(option == '+') {
+			int num1 = addAndSub.getNumber();
+			int num2 = addAndSub.getNumber();
+			int sum = addAndSub.add(num1, num2);
+			addAndSub.print(sum);
+			while(save == 'y') {
+				System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+				save = keyboard.next().charAt(0);
+				if(save == 'y') {
+					update(sum, updated);
+				}
+				else if(save == 'n') {
+					main(args);
+				}
+				else {
+					while(save != 'y' || save != 'n') {
+						System.out.println("Error. Enter either y or n");
+						System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+						save = keyboard.next().charAt(0);
 					}
 				}
 			}
-			if(option == '-') {
-				int num1 = addAndSub.getNumber();
-				int num2 = addAndSub.getNumber();
-				int difference = addAndSub.sub(num1, num2);
-				addAndSub.print(difference);
-				while(save == 'y') {
-					System.out.println("Would you like to save this number for your next calculation? (y/n)?");
-					save = keyboard.next().charAt(0);
-					if(save != 'n') {
-						update(difference, updated);
+		}
+		else if(option == '-') {
+			int num1 = addAndSub.getNumber();
+			int num2 = addAndSub.getNumber();
+			int difference = addAndSub.sub(num1, num2);
+			addAndSub.print(difference);
+			while(save == 'y') {
+				System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+				save = keyboard.next().charAt(0);
+				if(save == 'y') {
+					update(difference, updated);
+				}
+				else if(save == 'n') {
+					main(args);
+				}
+				else {
+					while(save != 'y' || save != 'n') {
+						System.out.println("Error. Enter either y or n");
+						System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+						save = keyboard.next().charAt(0);
 					}
 				}
+			}
 			
-			}
+		}
 			
-			if(option == '*') {
-				int num1 = addAndSub.getNumber();
-				int num2 = addAndSub.getNumber();
-				int product = multAndDiv.multiply(num1, num2);
-				addAndSub.print(product);
-				while(save == 'y') {
-					System.out.println("Would you like to save this number for your next calculation? (y/n)?");
-					save = keyboard.next().charAt(0);
-					if(save != 'n') {
-						update(product, updated);
+		else if(option == '*') {
+			int num1 = addAndSub.getNumber();
+			int num2 = addAndSub.getNumber();
+			int product = multAndDiv.multiply(num1, num2);
+			addAndSub.print(product);
+			while(save == 'y') {
+				System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+				save = keyboard.next().charAt(0);
+				if(save == 'y') {
+					update(product, updated);
+				}
+				else if(save == 'n') {
+					main(args);
+				}
+				else {
+					while(save != 'y' || save != 'n') {
+						System.out.println("Error. Enter either y or n");
+						System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+						save = keyboard.next().charAt(0);
 					}
 				}
 			}
+		}
 			
-			if(option == '/') {
-				int num1 = addAndSub.getNumber();
-				int num2 = addAndSub.getNumber();
-				int quotient = multAndDiv.divide(num1, num2);
-				addAndSub.print(quotient);
-				while(save == 'y') {
-					System.out.println("Would you like to save this number for your next calculation? (y/n)?");
-					save = keyboard.next().charAt(0);
-					if(save != 'n') {
-						update(quotient, updated);
+		else if(option == '/') {
+			int num1 = addAndSub.getNumber();
+			int num2 = addAndSub.getNumber();
+			int quotient = multAndDiv.divide(num1, num2);
+			addAndSub.print(quotient);
+			while(save == 'y') {
+				System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+				save = keyboard.next().charAt(0);
+				if(save == 'y') {
+					update(quotient, updated);
+				}
+				else if(save == 'n') {
+					main(args);
+				}
+				else {
+					while(save != 'y' || save != 'n') {
+						System.out.println("Error. Enter either y or n");
+						System.out.println("Would you like to save this number for your next calculation? (y/n)?");
+						save = keyboard.next().charAt(0);
 					}
 				}
 			}
-			if(option == '!') {
-				System.out.println("Goodbye!");
-				System.exit(0);
-			}
+		}
+		else if(option == '!') {
+			System.out.println("Goodbye!");
+			System.exit(0);
+		}
+		else {
+			System.out.println("Error must use one of these characters: +, -, *, /");
+		}
 		}while(cont == true);
 	}
 
@@ -90,21 +133,25 @@ public class Calculator {
 			addAndSub.print(sum2);
 		}
 		
-		if(operation == '-') {
+		else if(operation == '-') {
 			int num2 = addAndSub.getNumber();
 			int diff = addAndSub.sub(newNum, num2);
 			addAndSub.print(diff);
 		}
 		
-		if(operation == '*') {
+		else if(operation == '*') {
 			int num2 = addAndSub.getNumber();
 			int pro = multAndDiv.multiply(newNum, num2);
 			addAndSub.print(pro);
 		}
-		if(operation == '/') {
+		else if(operation == '/') {
 			int num2 = addAndSub.getNumber();
 			int quo = multAndDiv.divide(newNum, num2);
 			addAndSub.print(quo);
+		}
+		else {
+			System.out.println("Error must use one of these characters: +, -, *, /");
+			update(total, newNum);
 		}
 	}
 	
